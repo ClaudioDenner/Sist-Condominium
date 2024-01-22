9beed66e61e6bf671d645a9e2e8efd9db23cbfcd
Claudio Denner <claudiodenner2@gmail.com>
2024-01-22 13:56:03 -0300

    feat: criando módulo de users

--- backend/backend/src/app.module.ts
+++ backend/backend/src/app.module.ts
@@ -3,8 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule],
+  imports: [ConfigModule.forRoot(), AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
