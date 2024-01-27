4147d94da8a027e9b200a384017551e8b7b7b277
Claudio Denner <claudiodenner2@gmail.com>
2024-01-27 07:39:42 -0300

    feat: adicionando foreign key em tabela informations.

---
+++ backend/backend/db/typeORM/migrations/1706349151762-migrate.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class Migrate1706349151762 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
