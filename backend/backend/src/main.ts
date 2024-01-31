30b3df91dca2a8645e26108996dcb17ef53eef0c
Claudio Denner <claudiodenner2@gmail.com>
2024-01-30 21:49:46 -0300

    feat: habilitando CORS na aplicação

--- backend/backend/src/main.ts
+++ backend/backend/src/main.ts
@@ -5,6 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: '*' /*in production set whats domain can access this api */,
+    allowedHeaders: 'Content-Type,Authorization',
+  });
   await app.listen(3000);
 }
 bootstrap();
