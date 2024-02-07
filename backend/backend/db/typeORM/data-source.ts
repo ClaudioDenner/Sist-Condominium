c80cee9d6907521ea02304ea46ea204e04894402
Claudio Denner <claudiodenner2@gmail.com>
2024-02-06 21:23:44 -0300

    feat: install dotenv and config enviromnent variables for data-source

--- backend/backend/db/typeORM/data-source.ts
+++ backend/backend/db/typeORM/data-source.ts
@@ -1,12 +1,13 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { SeederOptions } from 'typeorm-extension';
+import 'dotenv/config';
 
 export const options: DataSourceOptions & SeederOptions = {
   type: 'mysql',
   host: process.env.DB_HOST,
   port: +process.env.DB_PORT,
   username: process.env.DB_ROOT,
-  password: process.env.PASS,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   migrations: [`${__dirname}/migrations/**/*.ts`],
 };
